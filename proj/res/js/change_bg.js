cd3a0ab7fa2d5c430483567137bbbc40f84b01f1
Albert Berges Peiro <albertbergespeiro@Alberts-MacBook-Pro.local>
2015-10-29 22:34:13 +0100

    Project upload

---
+++ proj/res/js/change_bg.js
@@ -0,0 +1,43 @@
+(function( $ ) {
+
+	//Function to animate slider captions 
+	function doAnimations( elems ) {
+		//Cache the animationend event in a variable
+		var animEndEv = 'webkitAnimationEnd animationend';
+		
+		elems.each(function () {
+			var $this = $(this),
+				$animationType = $this.data('animation');
+			$this.addClass($animationType).one(animEndEv, function () {
+				$this.removeClass($animationType);
+			});
+		});
+	}
+	
+	//Variables on page load 
+	var $myCarousel = $('#myCarousel'),
+		$firstAnimatingElems = $myCarousel.find('.item:first').find("[data-animation ^= 'animated']");
+		
+	//Initialize carousel 
+	$myCarousel.carousel();
+	
+	//Animate captions in first slide on page load 
+	doAnimations($firstAnimatingElems);
+	
+	//Pause carousel  
+	$myCarousel.carousel('pause');
+	
+	//Colors for background
+	var colors = ["#FF3333","#5BD6FF","#5CD65C"];                          
+	
+	//Other slides to be animated on carousel slide event 
+	$myCarousel.on('slide.bs.carousel', function (e) {
+		var $animatingElems = $(e.relatedTarget).find("[data-animation ^= 'animated']");
+		doAnimations($animatingElems);
+
+		//Change background color
+		var rand = Math.floor(Math.random()*colors.length); 
+		$("body").css("background-color",colors[rand]);
+	});  
+	
+})(jQuery);
